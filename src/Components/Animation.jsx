28515a2696554e7d44f76d71fb1528611c592b15
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-18 21:06:06 +0530

    added animation

---
+++ src/Components/Animation.jsx
@@ -0,0 +1,14 @@
+function Animation() {
+  return (
+    <div id="animation">
+      <div id="box">
+        <div class="triangle" id="triangle1"></div>
+        <div class="triangle" id="triangle2"></div>
+        <div class="triangle" id="triangle3"></div>
+        <div class="triangle" id="triangle4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Animation;
--- src/Components/Page3.jsx
+++ src/Components/Page3.jsx
@@ -1,6 +1,7 @@
 function Page3() {
   return (
     <div id="page3">
+      <div className="background"></div>
       <div id="leftImage">
         <img src="https://s3-alpha-sig.figma.com/img/cb32/eb69/29dc45e5c648ff07000ce1feedef6ad2?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ixN0kJMHrGx48~4hSlRBodadAwzCCJ--pJ4PFMny2gbjtqoZEgfLLeeHCdtlNVWbJAYBvglYqos~be~wniMo5iIUjA-8KDly5v17ceqoCW4ty7jDPuVWCry-s6ro0sVSWbwHTTclnBs53WW0p~vSk4bBzte63IVOOVN9t3e4m4xRqiWasdUORKIWXId86tvMW7ErYlNPAxHiLMcurMzWT6LEGjZQguXHySF~sOUyrwm1f-CMRVWkhCB-vhjnLquGdPbkgl-~BQI3gal10crBRQGDB2h21c~baFy6CUKGQspkhmPckHf3c242gS~uZyGpt8zA7tMgsyM4pyMqLU4s9g__" />
       </div>
