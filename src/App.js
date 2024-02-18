1256633bd707658626a3394349f0db10a7f1b731
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-18 21:26:46 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import NavBar from "./Components/NavBar";
 import Page1 from "./Components/Page1";
 import Page2 from "./Components/Page2";
 import Page3 from "./Components/Page3";
-import Page4 from "./Components/Page4";
+import Animation from "./Components/Animation";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <div id="background"></div>
+      <div className="background"></div>
       <NavBar />
       <Page1 />
       <Page2 />
       <Page3 />
-      <Page4 />
+      <Animation />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <div id="footer">
+      <div id="redi">
+        <p>Home</p>
+        <p>Page 1</p>
+        <p>Page 2</p>
+      </div>
+      <div id="socialMedia">
+        <p>FaceBook</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+        <p>Tik Tok</p>
+      </div>
+      <div id="copyright">Logo, Inc.</div>
+    </div>
+  );
+}
+
+export default Footer;
