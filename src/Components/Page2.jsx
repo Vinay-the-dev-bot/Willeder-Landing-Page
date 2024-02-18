34c4f8e6fdc792b01634c97aa073871a25aafe7c
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-18 20:29:33 +0530

    page 2 and page 3 completed

---
+++ src/Components/Page2.jsx
@@ -0,0 +1,27 @@
+function Page2() {
+  return (
+    <div id="page2">
+      <h1>LOREM</h1>
+      <p>subtitle</p>
+      <div id="carousal">
+        <div id="carousal1">
+          <p>01</p>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div id="carousal2">
+          <p>02</p>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div id="carousal3">
+          <p>03</p>
+          <p>Lorem ipsum dolor sit</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page2;
---
+++ src/Components/Page3.jsx
@@ -0,0 +1,26 @@
+function Page3() {
+  return (
+    <div id="page3">
+      <div id="leftImage">
+        <img src="https://s3-alpha-sig.figma.com/img/cb32/eb69/29dc45e5c648ff07000ce1feedef6ad2?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ixN0kJMHrGx48~4hSlRBodadAwzCCJ--pJ4PFMny2gbjtqoZEgfLLeeHCdtlNVWbJAYBvglYqos~be~wniMo5iIUjA-8KDly5v17ceqoCW4ty7jDPuVWCry-s6ro0sVSWbwHTTclnBs53WW0p~vSk4bBzte63IVOOVN9t3e4m4xRqiWasdUORKIWXId86tvMW7ErYlNPAxHiLMcurMzWT6LEGjZQguXHySF~sOUyrwm1f-CMRVWkhCB-vhjnLquGdPbkgl-~BQI3gal10crBRQGDB2h21c~baFy6CUKGQspkhmPckHf3c242gS~uZyGpt8zA7tMgsyM4pyMqLU4s9g__" />
+      </div>
+      <div id="content">
+        <h1>Lorem Ipsum</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum{" "}
+          <p>
+            <span class="material-symbols-outlined">arrow_right_alt</span>
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Page3;
