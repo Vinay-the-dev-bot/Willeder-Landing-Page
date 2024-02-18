370873537e9f94d83a495f1e7f9806a64cd2e5ee
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-18 19:41:27 +0530

    Completed Page one

---
+++ src/Components/Page1.js
@@ -0,0 +1,19 @@
+import "../Images/044cb65dd089cf0863aeb45d88915340.jpg";
+function Page1() {
+  return (
+    <div id="page1">
+      <p id="para1">LOGO</p>
+      <p id="para2">lorem ipsum dolor sit amet</p>
+      <p id="para3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </p>
+      <p id="para4">incididunt ut labore et dolore magna aliqua.</p>
+      <p>scroll</p>
+      <div> </div>
+      {/* <img src="https://s3-alpha-sig.figma.com/img/754a/f64a/7259f8ece71ae230e41c3a2c6709f5db?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H9xvtUVkyc~hjRApIiHndhkun3UakhIsW-8Ri9-U2cMH9FlwY~hahLDE6KWJRTJRTs3gwfydIPQkx8NoXFixJHkW2MV2BCjOjXNugCyHBdQxwyX2mhorOojgD5GwLGaWmsRr15zJvF~n5KCcnvudKbbQBuehqAqIqf1asEspkLWCsAq2uVbDUST8-jhFPMFgZ4Jp5F5AZFfftqKnFAh2SXDSg~5OJvNOJMh5bDQ9DvpEpV7IuF5hmCCvRT-MUuozKKuJV4kLq0~XNe72QqIBoMY2NXmou5hTIdCWdt9krHRUG411TvQg8NNbgXDTq~CK5kxdKU6HfaYHW-cHbddUqg__"></img> */}
+    </div>
+  );
+}
+
+export default Page1;
