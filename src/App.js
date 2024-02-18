12894019f5bc81bbe709fee3c01a746eb338bfe7
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-18 18:50:16 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./Components/NavBar";
+import Page1 from "./Components/Page1";
+import Page2 from "./Components/Page2";
+import Page3 from "./Components/Page3";
+import Page4 from "./Components/Page4";
 
 function App() {
   return (
     <div className="App">
-      <h1>Willeder Landing Page</h1>
+      <div id="background"></div>
+      <NavBar />
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <nav id="navbar">
+      <div id="logo">LOGO</div>
+      <div>
+        <ul id="nav">
+          <li>Home</li>
+          <li>Page 1</li>
+          <li>Page 2</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
